75552ed73930b436b53bc9c55dc78873a7258bc5
ggruessing <ghgruessing@gmail.com>
2017-08-02 13:24:06 -0700

    RAZZLENDAZZLE

---
+++ models/raffles.js
@@ -0,0 +1,52 @@
+var db = require("../models")
+
+module.exports = function(sequelize,DataTypes) {
+
+    var raffles = sequelize.define("raffles" , {
+      item: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+      link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+      itemPrice: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 100,
+      },
+      totalTickets: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 100
+      },
+      purchasedTickets: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      },
+      ticketPrice:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: function(){
+          db.raffles.findOne({where: {id: (id)}}).then(function(response){
+            return (response.itemPrice / response.totalTickets)
+            })
+         }  
+      },
+      won: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    } 
+  },{timestamps: false}) 
+
+  raffles.associate = function(models){
+
+      raffles.hasMany(models.tickets,{
+        onDelete: "cascade"
+      })
+  }
+  return raffles  
+};
